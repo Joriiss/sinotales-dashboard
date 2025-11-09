0d8f76cee257167a1279b83db851b901d4b8a0fa
Joriiss <78536901+Joriiss@users.noreply.github.com>
2025-11-09 10:52:07 +0100

    Update ALLOWED_HOSTS in settings.py to include localhost for local development access.

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-this-in
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dashboard.joris-rabilloud.com']
+ALLOWED_HOSTS = ['dashboard.joris-rabilloud.com', '127.0.0.1']
 
 
 # Application definition
